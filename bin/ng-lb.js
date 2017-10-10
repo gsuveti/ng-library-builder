88cee9b1980788a9ec1bc3f993478068abe61ccb
georgesuveti <george.suveti@irian.ro>
2017-10-10 20:44:18 +0300

    add bin script

---
+++ bin/ng-lb.js
@@ -0,0 +1,19 @@
+#! /usr/bin/env node
+var argv = require('yargs').argv;
+var shell = require("shelljs");
+var path = require('path');
+
+if (argv.rootDir) {
+    const ngLibraryBuilderDirAsArray = ['node_modules', '@irian-ro', 'ng-library-builder'];
+    const ngLibraryDir = path.join(...ngLibraryBuilderDirAsArray);
+
+    const rootDir = path.join(...ngLibraryBuilderDirAsArray.fill('..'), argv.rootDir);
+    const outDir = path.join(...ngLibraryBuilderDirAsArray.fill('..'), argv.outDir ? argv.outDir : 'dist');
+
+    const task = argv.publish ? 'publish' : 'build';
+    shell.exec(`cd ${ngLibraryDir} && gulp ${task} --rootDir ${rootDir} --outDir ${outDir}`);
+} else {
+    console.error("rootDir not defined");
+}
+
+
